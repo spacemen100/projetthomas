79b555e31e20131a84dc6ca913c9be9a80e6ad70
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-01-19 11:03:23 +0000

    vgffddfgfgfg

---
+++ src/indexx.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AdminLayout from 'layouts/admin';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme/theme';
+import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
+
+const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <React.StrictMode>
+        <ThemeEditorProvider>
+          <HashRouter>
+            <Switch>
+              <Route path={`/admin`} component={AdminLayout} />
+              <Redirect from='/' to='/admin' />
+            </Switch>
+          </HashRouter>
+        </ThemeEditorProvider>
+      </React.StrictMode>
+    </ChakraProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
