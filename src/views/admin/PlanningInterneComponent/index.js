b753ea16ea42479345a504d757a1c63c9aff6793
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-01-20 10:46:54 +0000

    gbncvnbnhnhgh

--- src/views/admin/PlanningInterneComponent/index.js
+++ src/views/admin/PlanningInterneComponent/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {
-  Box,
-
-} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import App from './components/App';
 
 export default function Partner() {
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-      <App/>
+      <App />
     </Box>
   );
 }
-
