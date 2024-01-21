bc13a55943bd9653622b0ef3743d7025247cbf94
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-01-21 22:09:40 +0000

    gbgfnnhnhhg

--- src/views/admin/PlanningInterneComponent/components/App.js
+++ src/views/admin/PlanningInterneComponent/components/App.js
@@ -17,29 +17,38 @@ export default function Component() {
         .from('vianney_actions')
         .select('*');
 
-      if (error) console.log("error", error);
-      else {
-        // Transform the data to fit the SchedulerData structure
-        const transformedData = vianneyActions.map(action => ({
-          id: action.id,
-          label: {
-            icon: action.photo_profile_url,
-            title: action.nom,
-            subtitle: action.statut_dans_la_boite
-          },
-          data: [
-            {
-              id: action.id,
-              startDate: new Date(action.starting_date),
-              endDate: new Date(action.ending_date),
-              title: action.action_name,
-              description: action.action_comment,
-              subtitle: action.name_of_the_client_that_reserved_it,
-              bgColor: action.color
-            }
-          ]
-        }));
+      if (error) {
+        console.log("error", error);
+      } else {
+        // Group actions by team
+        const groupedData = vianneyActions.reduce((acc, action) => {
+          if (!acc[action.team_to_which_its_attached]) {
+            acc[action.team_to_which_its_attached] = {
+              id: action.team_to_which_its_attached,
+              label: {
+                icon: action.photo_profile_url,
+                title: action.nom,
+                subtitle: action.statut_dans_la_boite
+              },
+              data: []
+            };
+          }
 
+          acc[action.team_to_which_its_attached].data.push({
+            id: action.id,
+            startDate: new Date(action.starting_date),
+            endDate: new Date(action.ending_date),
+            title: action.action_name,
+            description: action.action_comment,
+            subtitle: action.name_of_the_client_that_reserved_it,
+            bgColor: action.color
+          });
+
+          return acc;
+        }, {});
+
+        // Convert grouped data into array format
+        const transformedData = Object.values(groupedData);
         setData(transformedData);
       }
       setLoading(false);
