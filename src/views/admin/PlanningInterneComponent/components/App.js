b0150d0b9e9502c4fe0793cc6bef9ccd31451dc4
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-01-21 22:02:18 +0000

    fbgdfghhfghgfhgfgh

--- src/views/admin/PlanningInterneComponent/components/App.js
+++ src/views/admin/PlanningInterneComponent/components/App.js
@@ -1,12 +1,52 @@
-import {  useState } from "react";
+import { useState, useEffect } from "react";
+import supabase from './../../../../supabaseClient';
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
-import "dayjs/locale/fr"; // Import the French locale
-import { Scheduler, SchedulerData } from "@spacemen1000/react-scheduler";
+import "dayjs/locale/fr";
+import { Scheduler } from "@spacemen1000/react-scheduler";
 dayjs.extend(isBetween);
 
 export default function Component() {
-  const [isLoading] = useState(false); 
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      let { data: vianneyActions, error } = await supabase
+        .from('vianney_actions')
+        .select('*');
+
+      if (error) console.log("error", error);
+      else {
+        // Transform the data to fit the SchedulerData structure
+        const transformedData = vianneyActions.map(action => ({
+          id: action.id,
+          label: {
+            icon: action.photo_profile_url,
+            title: action.nom,
+            subtitle: action.statut_dans_la_boite
+          },
+          data: [
+            {
+              id: action.id,
+              startDate: new Date(action.starting_date),
+              endDate: new Date(action.ending_date),
+              title: action.action_name,
+              description: action.action_comment,
+              subtitle: action.name_of_the_client_that_reserved_it,
+              bgColor: action.color
+            }
+          ]
+        }));
+
+        setData(transformedData);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   // Set the locale to French
   dayjs.locale("fr");
@@ -28,7 +68,7 @@ export default function Component() {
   return (
     <section>
       <Scheduler
-        data={mockedSchedulerData}
+        data={data}
         isLoading={isLoading}        
         onItemClick={handleItemClick}
         onFilterData={handleFilterData}
@@ -37,61 +77,9 @@ export default function Component() {
           maxRecordsPerPage: 5,
           filterButtonState: -1,
           includeTakenHoursOnWeekendsInDayView: false,
-          lang:'fr',
+          lang: 'fr',
         }}
       />
     </section>
   );
 }
-
-const mockedSchedulerData: SchedulerData = [
-  {
-    id: "070ac5b5-8369-4cd2-8ba2-0a209130cc60",
-    label: {
-      icon: "https://picsum.photos/24",
-      title: "Joe Doe",
-      subtitle: "Frontend Developer"
-    },
-    data: [
-      {
-        id: "8b71a8a5-33dd-4fc8-9caa-b4a584ba3762",
-        startDate: new Date("2023-04-13T15:31:24.272Z"),
-        endDate: new Date("2023-08-28T10:28:22.649Z"),
-        occupancy: 3600,
-        title: "Project A",
-        subtitle: "Subtitle A",
-        description: "array indexing Salad West Account",
-        bgColor: "rgb(254,165,177)"
-      },
-      {
-        id: "22fbe237-6344-4c8e-affb-64a1750f33bd",
-        startDate: new Date("2023-10-07T08:16:31.123Z"),
-        endDate: new Date("2023-11-15T21:55:23.582Z"),
-        occupancy: 2852,
-        title: "Project B",
-        subtitle: "Subtitle B",
-        description: "Tuna Home pascal IP drive",
-        bgColor: "rgb(254,165,177)"
-      },
-      {
-        id: "3601c1cd-f4b5-46bc-8564-8c983919e3f5",
-        startDate: new Date("2023-03-30T22:25:14.377Z"),
-        endDate: new Date("2023-09-01T07:20:50.526Z"),
-        occupancy: 1800,
-        title: "Project C",
-        subtitle: "Subtitle C",
-        bgColor: "rgb(254,165,177)"
-      },
-      {
-        id: "b088e4ac-9911-426f-aef3-843d75e714c2",
-        startDate: new Date("2023-10-28T10:08:22.986Z"),
-        endDate: new Date("2023-10-30T12:30:30.150Z"),
-        occupancy: 11111,
-        title: "Project D",
-        subtitle: "Subtitle D",
-        description: "Garden heavy an software Metal",
-        bgColor: "rgb(254,165,177)"
-      }
-    ]
-  }
-];
